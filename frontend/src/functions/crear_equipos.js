20071c50a3a6d593b8da6020177cdd46e3524c1c
Estefania <estefaniadelvalle0987@gmail.com>
2024-07-28 13:39:41 -0300

    formateo a archivos con prettier, validacion de nombre y manejo de errores al crear equipo

--- frontend/src/functions/crear_equipos.js
+++ frontend/src/functions/crear_equipos.js
@@ -15,45 +15,45 @@ function request_error(error) {
   alert(error);
 }
 
-function equipo_creado(data){
+function equipo_creado(data) {
   console.log(data);
 }
 
-function agregar_jugador(){
+function agregar_jugador() {
   jugadores = document.getElementById("jugadores");
   let div = document.createElement("div");
   let ingrese = document.createElement("h4");
   ingrese.textContent = "Ingrese un jugador";
   let input = document.createElement("input");
-  input.name = 'nombres';
-  input.placeholder = 'ingrese el nombre del jugador';
+  input.name = "nombres";
+  input.placeholder = "ingrese el nombre del jugador";
   div.appendChild(ingrese);
   div.appendChild(input);
   jugadores.appendChild(div);
 }
 
-function subir_equipo(datos){
-  fetch("http://localhost:5000/crear_equipo" ,{
-    method: 'POST',
+function subir_equipo(datos) {
+  fetch("http://localhost:5000/crear_equipo", {
+    method: "POST",
     headers: {
-    'Content-Type': 'application/json'
-  },
-    body: JSON.stringify(datos)
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(datos),
   })
-  .then((res) => res.json())
-  .then(equipo_creado)
-  .catch(request_error)
+    .then((res) => res.json())
+    .then(equipo_creado)
+    .catch(request_error);
 }
 
-function cruces_creados(data){
+function cruces_creados(data) {
   console.log(data);
 }
 
-function crear_cruces(){
+function crear_cruces() {
   fetch(`http://localhost:5000/generar_fechas/${id}`)
-  .then((res) => res.json())
-  .then(cruces_creados)
-  .catch(request_error)
+    .then((res) => res.json())
+    .then(cruces_creados)
+    .catch(request_error);
 }
 
 function sleep(milliseconds) {
@@ -64,57 +64,59 @@ function sleep(milliseconds) {
   } while (currentDate - date < milliseconds);
 }
 
-function verificar_caso(){
-  if (equipo_actual<=cantidad_equipos){
+function verificar_caso() {
+  if (equipo_actual <= cantidad_equipos) {
     let nombre_equipo = document.getElementById("nombre");
     nombre_equipo.value = "";
     mostrar();
-  } else{
+  } else {
     sleep(500);
     crear_cruces();
-    alert("equipos creados con exito");
-    window.location.href = `http://localhost:5000/src/pages/administrar_torneo?id=${id}`;
+    alert("Equipos creados con exito");
+    window.location.href = `http://localhost/src/pages/administrar_torneo?id=${id}`;
   }
 }
 
-function actualizar(){
+function actualizar() {
   equipo_actual += 1;
   var datos = {
-    nombre : document.getElementById("nombre").value,
-    torneo : id,
-    guardar : jugadores_guardar,
-    jugadores : []
+    nombre: document.getElementById("nombre").value,
+    torneo: id,
+    guardar: jugadores_guardar,
+    jugadores: [],
   };
-  if (jugadores_guardar == 1){
+  if (jugadores_guardar == 1) {
     const nombres = document.querySelectorAll('input[name="nombres"]');
-    for (let i = 0; i<nombres.length;i++){
-      datos.jugadores.push(nombres[i].value);      
+    for (let i = 0; i < nombres.length; i++) {
+      datos.jugadores.push(nombres[i].value);
     }
   }
   subir_equipo(datos);
   verificar_caso();
 }
 
-function mostrar(){
-  titulo = document.getElementById("numero_equipo")
-  titulo.innerText = "equipo " + String(equipo_actual);
-  if (jugadores_guardar == 1){
+function mostrar() {
+  titulo = document.getElementById("numero_equipo");
+  titulo.innerText = "Equipo " + String(equipo_actual);
+  if (jugadores_guardar == 1) {
     jugadores = document.getElementById("jugadores");
     jugadores.innerHTML = "";
-    for (let i = 0; i<5;i++){
+    for (let i = 0; i < 5; i++) {
       agregar_jugador();
     }
     agregar = document.getElementById("boton");
     agregar.innerHTML = "";
     boton = document.createElement("button");
     boton.classList.add("botones");
-    boton.onclick = function() {agregar_jugador();};
+    boton.onclick = function () {
+      agregar_jugador();
+    };
     boton.textContent = "+";
     agregar.appendChild(boton);
   }
 }
 
-function procesar(data){
+function procesar(data) {
   cantidad_equipos = data.cantidad;
   equipo_actual = 1;
   jugadores_guardar = data.guardar;
@@ -122,6 +124,6 @@ function procesar(data){
 }
 
 fetch(`http://localhost:5000/ver_extras/${id}`)
-.then((res) => res.json())
-.then(procesar)
-.catch(request_error)
+  .then((res) => res.json())
+  .then(procesar)
+  .catch(request_error);
